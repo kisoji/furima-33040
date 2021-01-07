e82ba8111ac6b1d5a997ec511792fb7198cf0346
kisoji <kotooka149@yahoo.co.jp>
2021-01-07 15:16:41 +0900

    商品登録画面の遷移までActive未完

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :preparation_day_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 end
