8bddd52f4afe58e50dd81f5c1782e81390994b5d
kisoji <kotooka149@yahoo.co.jp>
2021-01-11 12:39:51 +0900

    情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,11 +22,23 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+   
+      @item.update(item_params) if @item.user.id == current_user.id
+    return  redirect_to item_path if @item.valid?
+    
+      render 'edit'
+  end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :category_id, :condition_id, :delivery_id, :prefecture_id,
-                                 :preparation_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :introduction, :category_id, :condition_id, :delivery_id, :prefecture_id, :preparation_id, :price, :image).merge(user_id: current_user.id)
   end
 end
