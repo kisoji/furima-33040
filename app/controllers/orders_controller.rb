53120bbd04359e6546d07199d9a16d04469344e7
kisoji <kotooka149@yahoo.co.jp>
2021-01-15 14:11:15 +0900

    Revert "Revert "クレジット決済での購入機能まで""
    This reverts commit 053c369cd81921a6dfff953808837f8ef13689af.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,14 +4,15 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     return redirect_to root_path if @item.user.id == current_user.id || @item.order != nil
-    @order = Item.find(params[:item_id])
+    # @order = Item.find(params[:item_id])
     @user_order = UserOrder.new
   end
 
   def create
-    @order = Item.find(params[:item_id])
+    @item = Item.find(params[:item_id])
     @user_order = UserOrder.new(order_params)
     if @user_order.valid?
+      pay_item
       @user_order.save
       redirect_to root_path
     else
@@ -22,6 +23,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:user_order).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:user_order).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+
 end
